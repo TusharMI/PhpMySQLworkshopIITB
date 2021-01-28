fb0ba3e3828144d0c88c677e134b7ae3ff83c697
TUSHAR KUNJIR <tusharkunjir08@gmail.com>
2021-01-29 00:31:27 +0530

    first commit

---
+++ day_1/q2_1_operators.php
@@ -0,0 +1,9 @@
+<?php
+
+	$x=100;
+	$y=60;
+	echo "The addition of x and y is : ". ($x+$y) ."<br />";
+	echo "The substraction between x and y is : ". ($x-$y) ."<br />";
+	echo "Multiplication of x and y : ". ($x*$y) ."<br />";
+	echo "Division of x and y : ". ($x/$y) ."<br />";
+?>
