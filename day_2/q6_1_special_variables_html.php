448da16efa3dca816a94e7dc73173a3899c121af
TUSHAR KUNJIR <tusharkunjir08@gmail.com>
2021-01-29 00:33:06 +0530

    second commit

---
+++ day_2/q6_1_special_variables_html.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<form action="q6_1_special_variables_html.php" method = 'GET' >
+	<input type='text' name= 'a'><br>
+	<input type='text' name= 'b'><br>
+	<input type='text' name= 'c'><br>
+	<input type='submit' value='Click here'><br>
+</form>
+
+</html>
+<?php
+
+if(isset($_GET['a']))
+	$a = $_GET['a'];
+else
+{
+	$a = "length not set";	
+}
+	
+if(isset($_GET['b']))
+{
+	$b = $_GET['b'];
+}
+else
+{
+	$b = "length not set";
+}
+
+if(isset($_GET['c']))
+{
+	$c = $_GET['c'];
+}
+else
+{
+	$c = "length not set";
+}	
+echo $a."<br>" ;;
+echo $b."<br>" ;;
+echo $c."<br>" ;;
+/*
+$a = $_GET['a'];
+if($a)
+echo "a =".$a ."<br>" ;
+
+$b = $_GET['b'];
+if($b)
+echo "b =".$b . "<br>" ;
+
+$c = $_GET['c'];
+if ($c)
+echo "c =".$c . "<br>" ;
+*/
+
+if(($a==$b) && ($b==$c))
+{
+echo("Equilateral triangle");
+}
+else
+if(($a==$b) || ($b==$c) || ($c==$a))
+{
+echo("Isosceles triangle");
+}
+else
+if(($a!=$b && $b!=$c && $c!=$a))
+{
+echo("Scalene triangle");
+}
+return 0
+
+
+
+
+?>
